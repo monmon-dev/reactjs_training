62d07768f17d7f764e6fe677171f48bbfbde6d70
Turbanada <reymond.turbanada@accenture.com>
2022-06-17 14:10:35 +0800

    Importing npm modules

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,6 @@
-// import "./utils";
-// import { square, add } from "./utils";
+// install -> import -> use
+import React from "react";
+import ReactDOM from "react-dom";
 
-// console.log("app.js is running");
-// console.log(square(4));
-// console.log(add(12, 12));
-// console.log(subtract(14, 12));
-
-import isSenior, { isAdult, canDrink } from "./person";
-console.log(isAdult(18));
-console.log(canDrink(20));
-console.log(isSenior(65));
+const template = React.createElement("p", {}, "testing 123");
+ReactDOM.render(template, document.getElementById("app"));
