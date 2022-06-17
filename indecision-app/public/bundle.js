6e106f3c5516b8ba69ebc5ca82a569d90221610b
Turbanada <reymond.turbanada@accenture.com>
2022-06-17 13:44:48 +0800

    Default exports

--- indecision-app/public/bundle.js
+++ indecision-app/public/bundle.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log(!0),console.log(!1)})();
+(()=>{"use strict";console.log(!0),console.log(!1),console.log(!0)})();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,7 +4,9 @@
 // console.log("app.js is running");
 // console.log(square(4));
 // console.log(add(12, 12));
+// console.log(subtract(14, 12));
 
-import { isAdult, canDrink } from "./person";
+import isSenior, { isAdult, canDrink } from "./person";
 console.log(isAdult(18));
 console.log(canDrink(20));
+console.log(isSenior(65));
